89675a91db9c35090815ab6460016c212b1b7a37
lee <lee@srpost.co.kr>
2026-05-15 13:25:12 +0900

    .env 호출 변경

--- src/main/java/com/api/ApiApplication.java
+++ src/main/java/com/api/ApiApplication.java
@@ -2,17 +2,9 @@ package com.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class ApiApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
-                .directory("./src")
-                .load();
-        dotenv.entries().forEach(e -> 
-            System.setProperty(e.getKey(), e.getValue())
-        );
         
         SpringApplication.run(ApiApplication.class, args);
     }
