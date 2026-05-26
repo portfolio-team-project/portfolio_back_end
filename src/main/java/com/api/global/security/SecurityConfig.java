41d9d12d9a901de9af5e752c235dbae9d35a50e4
EG-L <pshowx12@naver.com>
2026-05-26 23:47:12 +0900

    CORS preflight 체크

--- src/main/java/com/api/global/security/SecurityConfig.java
+++ src/main/java/com/api/global/security/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(corsProperties.getAllowedOrigin()));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
