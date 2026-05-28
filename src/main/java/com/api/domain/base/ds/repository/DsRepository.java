13ed84c738aa34f51b0769a1358146cb80e14f9f
lee <81463782+EG-L@users.noreply.github.com>
2026-05-29 08:55:21 +0900

    테스트 엔드포인트 삭제

--- src/main/java/com/api/domain/base/ds/repository/DsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.domain.base.ds.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.domain.base.ds.entity.DS;
-
-public interface DsRepository extends JpaRepository<DS, Integer>{
-    
-}
