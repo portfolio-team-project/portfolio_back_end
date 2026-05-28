0b45a71b3b33488f6c550f85eff65c3a022e3d53
lee <81463782+EG-L@users.noreply.github.com>
2026-05-28 16:12:40 +0900

    SMTP 추가 비밀번호 유효성 검증 추가

--- src/main/java/com/api/domain/base/Member/dto/ChangePasswordRequest.java
+++ src/main/java/com/api/domain/base/Member/dto/ChangePasswordRequest.java
@@ -1,5 +1,7 @@
 package com.api.domain.base.Member.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,5 +9,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ChangePasswordRequest {
     private String userId;
+    @NotBlank(message="비밀번호는 필수값입니다.")
+    @Pattern(
+        regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$",
+        message = "비밀번호는 8자 이상, 영문, 숫자, 특수문자를 포함해야 합니다."
+    )
     private String newPassword;
 }
