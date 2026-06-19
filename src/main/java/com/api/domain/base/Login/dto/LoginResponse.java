c8c0fdec57ef9a88b8beb775813123e24affa0ea
EG-L <pshowx12@naver.com>
2026-06-19 11:53:37 +0900

    직렬화 문제 수정

--- src/main/java/com/api/domain/base/Login/dto/LoginResponse.java
+++ src/main/java/com/api/domain/base/Login/dto/LoginResponse.java
@@ -1,5 +1,7 @@
 package com.api.domain.base.Login.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,5 +15,7 @@ public class LoginResponse {
 	private String userId;
 	private String userName;
 	private String role;
+	
+	@JsonProperty("isSocial")
 	private boolean isSocial;
 }
